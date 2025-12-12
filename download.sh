99720ff78936d0db81494eac3e6ad98128a58b55
Simon Binder <simon@journeyapps.com>
2025-12-12 10:54:06 +0100

    Remove sqlite3ext.h

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@ cd tmp
 curl -o amalgamation.zip -L https://github.com/utelle/SQLite3MultipleCiphers/releases/download/v2.2.6/sqlite3mc-2.2.6-sqlite-3.51.1-amalgamation.zip
 unzip amalgamation.zip
 
-mv sqlite3.h sqlite3ext.h ../Sources/CSQLite/include/
+mv sqlite3.h ../Sources/CSQLite/include/
 mv sqlite3.c ../Sources/CSQLite/
 
 mv sqlite3mc_amalgamation.h ../Sources/CSQLite3MultipleCiphers/include/sqlite3.h
