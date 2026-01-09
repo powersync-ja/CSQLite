28e8b680457046ce3e733e284567ef85475c60e2
Simon Binder <simon@journeyapps.com>
2026-01-09 13:53:50 +0100

    Fix download script

--- download.sh
+++ download.sh
@@ -5,11 +5,11 @@ cd tmp
 curl -o amalgamation.zip -L https://github.com/utelle/SQLite3MultipleCiphers/releases/download/v2.2.6/sqlite3mc-2.2.6-sqlite-3.51.1-amalgamation.zip
 unzip amalgamation.zip
 
-mv sqlite3.h ../Sources/CSQLite/include/
-mv sqlite3.c ../Sources/CSQLite/
+mv sqlite3.h ../Sources/CSQLite/_sqlite/
+mv sqlite3.c ../Sources/CSQLite/_sqlite/
 
-mv sqlite3mc_amalgamation.h ../Sources/CSQLite3MultipleCiphers/include/sqlite3.h
-mv sqlite3mc_amalgamation.c ../Sources/CSQLite3MultipleCiphers/
+mv sqlite3mc_amalgamation.h ../Sources/CSQLite/_sqlite3mc/sqlite3.h
+mv sqlite3mc_amalgamation.c ../Sources/CSQLite/_sqlite3mc/
 
 cd ..
 rm -r tmp
